abb11cd971694390237070420a921ca01e21c76e
Abhishek <126752231+abhishek82089@users.noreply.github.com>
2024-06-04 00:20:36 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,6 +1,6 @@
-api_id = "424428"
-api_hash = "dfa81e942e2b8d935a7f2a000192dec0"
-bot_token = "6317224996:AAENPXX1Rz75_EjjDqXja1x3ci5c_CsHwRU"
+api_id = "21311076"
+api_hash = "7e542efdc73d08a4b382b5d6754c072d"
+bot_token = "7438982903:AAE9r3OqVTCJV5Km2BNbq7V_tgEtnmHK9nk"
 #log_channel = -1002053569770
 sudo_groups = -1002096860446
-#auth_users = [942518345, 5441295351, 5277564931]
+#auth_users = [5483260471, 5441295351, 5277564931]
